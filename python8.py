0e1b3e9d94518e201dd74864ac66289d704aea47
gauravgoel11 <gaurav.agrwl.11@gmail.com>
2023-10-10 04:34:59 +0530

    commenting out all previos one

--- python8.py
+++ python8.py
@@ -1,21 +1,21 @@
 #first programm to print
-print('Hellow world')
+# print('Hellow world')
 
 #variables
-name = "gaurav"
-age =22
-print(name)
-print(age)
-is_adult = True
-print(is_adult)
+# name = "gaurav"
+# age =22
+# print(name)
+# print(age)
+# is_adult = True
+# print(is_adult)
 # name2 = input("what is your name ?")
 # print("Hello "+name2)
 
 #Type conversion
-old_age=float(input("Enter your old age : "))
-new_age=old_age + 2
-print(new_age)
+# old_age=float(input("Enter your old age : "))
+# new_age=old_age + 2
+# print(new_age)
 # float()
 # str()
 # bool()
-
+# print(5%2)
