62d5fce5298012f53a652f096406740454a9fcb6
gauravgoel11 <gaurav.agrwl.11@gmail.com>
2023-10-09 16:14:52 +0530

    one shot python in pyhon 8

---
+++ python8.py
@@ -0,0 +1,21 @@
+#first programm to print
+print('Hellow world')
+
+#variables
+name = "gaurav"
+age =22
+print(name)
+print(age)
+is_adult = True
+print(is_adult)
+# name2 = input("what is your name ?")
+# print("Hello "+name2)
+
+#Type conversion
+old_age=float(input("Enter your old age : "))
+new_age=old_age + 2
+print(new_age)
+# float()
+# str()
+# bool()
+
